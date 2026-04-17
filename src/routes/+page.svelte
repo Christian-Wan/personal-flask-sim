5bbb01aaceafd3e10932246949ade3edc064a869
Christian-Wan <145377201+Christian-Wan@users.noreply.github.com>
2026-04-17 17:17:55 -0400

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,7 +13,7 @@
 	import FluidSimulation from '$lib/FluidSimulation.svelte';
 
 	const MAX_GRAVITY = 9.81;
-	const MAX_GAS_GRAVITY = 5;
+	const MAX_GAS_GRAVITY = 9.81;
 
 
 	type AppState = 'loading' | 'needs-permission' | 'ready' | 'denied' | 'not-supported';
